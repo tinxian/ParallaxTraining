7b57a07295fd7d5f6e0bf0237aa4f69a7cd80a35
Tin Xian <43168595+tinxian@users.noreply.github.com>
2019-10-14 18:50:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let scrollAfstand; // checkt scrollafstand en geeft een getal
+let sectie2 = document.getElementsByClassName('sectie--2')[0];
+let sectie4 = document.getElementsByClassName('sectie--4')[0];
+let sectie6 = document.getElementsByClassName('sectie--6')[0];
+
+window.addEventListener('scroll', (e)=> {
+  scrollAfstand = window.pageYOffset;
+  console.log(scrollAfstand);
+    corrSection2(scrollAfstand);
+    corrSection4(scrollAfstand);
+    corrSection6(scrollAfstand);
+    if( scrollAfstand >= 600) {
+      verwijderClassKop();
+    }
+});
+
+const corrSection2 = (gescrolled) => {
+  sectie2.style.backgroundPositionY = -gescrolled/4 + "px";
+  sectie2.getElementsByClassName('section__kop')[0].style.marginLeft = gescrolled + 'px';
+}
+
+const corrSection4 = (gescrolled) => {
+  sectie4.style.backgroundPositionY = -gescrolled/3 + "px";
+}
+const corrSection6 = (gescrolled) => {
+  sectie6.style.backgroundPositionY = -gescrolled/2 + "px";
+}
+
+const verwijderClassKop = () => {
+  document.querySelector('.sectie--4 h2').classList.remove('sectie--4__kop');
+}
